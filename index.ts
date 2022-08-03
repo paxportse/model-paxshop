873afccc64f484c166a6bae429eb86b194bc3c22
Benjamin Josefsson <benjamin.josefsson@paxport.net>
2022-08-03 15:14:38 +0200

    Exported Booking.

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
+import { Booking } from "./Booking"
 import { BookingOptions } from "./BookingOptions"
 import { Cosmetic } from "./Cosmetic"
 import { Flight } from "./Flight"
 import { Luggage } from "./Luggage"
 import { Passenger } from "./Passenger"
 
-export { Cosmetic, Flight, BookingOptions, Luggage, Passenger }
+export { Cosmetic, Flight, Booking, BookingOptions, Luggage, Passenger }
