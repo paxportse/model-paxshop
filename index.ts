574b7b645f701f5a4b1b76f3bc4ddf1aad895e59
Amanda Hwatz Björkholm <amanda.hwatz@paxport.net>
2022-08-04 11:53:57 +0200

    Testing export of everything at top level

--- index.ts
+++ index.ts
@@ -4,8 +4,30 @@ import { BookingSpecifier } from "./BookingSpecifier"
 import { Client } from "./Client"
 import { Cosmetic } from "./Cosmetic"
 import { Flight } from "./Flight"
+import { Details } from "./Flight/Details"
+import { Meal } from "./Flight/Meal"
+import { Row } from "./Flight/Row"
 import { Seat } from "./Flight/Seat"
+import { Summary } from "./Flight/Summary"
 import { Luggage } from "./Luggage"
 import { Passenger } from "./Passenger"
+import { Itinerary } from "./Passenger/Itinerary"
+import { Price } from "./Price"
 
-export { Cosmetic, Flight, Booking, BookingOptions, BookingSpecifier, Client, Luggage, Passenger, Seat }
+export {
+	Cosmetic,
+	Flight,
+	Booking,
+	BookingOptions,
+	BookingSpecifier,
+	Client,
+	Itinerary,
+	Luggage,
+	Passenger,
+	Price,
+	Seat,
+	Summary,
+	Details,
+	Meal,
+	Row,
+}
