2ecaaa52e9a41303d8a88e7d52d4bfd1c9e51196
Amanda Hwatz Björkholm <amanda.hwatz@paxport.net>
2022-08-04 13:22:20 +0200

    Added bookingoptions to booking fetch.

--- Client/Booking.ts
+++ Client/Booking.ts
@@ -1,13 +1,14 @@
 import * as gracely from "gracely"
 import * as rest from "cloudly-rest"
 import { Booking as modelBooking } from "../Booking"
+import { BookingOptions } from "../BookingOptions"
 import { BookingSpecifier } from "../BookingSpecifier"
 
 export class Booking extends rest.Collection<gracely.Error> {
 	test(): boolean {
 		return true
 	}
-	fetch(specifier: BookingSpecifier): Promise<modelBooking | gracely.Error> {
-		return this.client.get<modelBooking>(`booking/${specifier.reference}`)
+	fetch(specifier: BookingSpecifier): Promise<(modelBooking & { options: BookingOptions }) | gracely.Error> {
+		return this.client.get<modelBooking & { options: BookingOptions }>(`booking/${specifier.reference}`)
 	}
 }
