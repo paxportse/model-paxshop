14a429ee3c54ba47d8c046284b6a006d81d2a0c2
Benjamin Josefsson <benjamin.josefsson@paxport.net>
2022-11-01 08:47:19 +0100

    Changed is-function and removed comments.

--- Meal/index.ts
+++ Meal/index.ts
@@ -1,19 +1,17 @@
 import { Alternative as MealAlternative } from "./Alternative"
-// import { Option } from "./Option"
 
 export interface Meal {
 	reference: string
 	name: string
 	optional?: boolean
 	alternatives: Meal.Alternative[]
-	//options?: Option[] // Vad används denna till?
 }
 
 export namespace Meal {
-	export function is(value: Meal): value is Meal {
+	export function is(value: Meal | any): value is Meal {
 		return (
 			typeof value == "object" &&
-			(value.reference == undefined || typeof value.reference == "string") && // Is this wrong? Reference should not be undefined.
+			typeof value.reference == "string" &&
 			typeof value.name == "string" &&
 			(value.optional == undefined || typeof value.optional == "boolean") &&
 			Array.isArray(value.alternatives) &&
