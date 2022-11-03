b1b22e25a5fc88ea9b579d8b2f17d3d629298c94
Benjamin Josefsson <benjamin.josefsson@paxport.net>
2022-11-03 14:08:08 +0100

    Added function to update passenger meals.

--- Meal/index.spec.ts
+++ Meal/index.spec.ts
@@ -1,8 +1,8 @@
-import * as model from "./index"
+import * as model from "../index"
 
 describe("model.Flight.Meal", () => {
 	const meal: model.Meal = {
-		name: "Dinner",
+		name: "dinner",
 		reference: "ref-034",
 		optional: true,
 		alternatives: [
@@ -10,6 +10,26 @@ describe("model.Flight.Meal", () => {
 			{ name: "meat", price: { amount: 12, currency: "DKK" } },
 		],
 	}
+	const meal2: model.Meal = {
+		name: "Dinner",
+		reference: "D-13",
+		optional: true,
+		alternatives: [{ name: "chicken", price: { amount: 100, currency: "SEK" } }],
+	}
+	const alternative: model.Meal.Alternative = { name: "chicken", price: { amount: 100, currency: "SEK" } }
+
+	const leg: model.Passenger.Itinerary.Leg = {
+		reference: "leg-02",
+		seat: {
+			row: { number: 1 },
+			position: "A",
+			status: "available",
+			class: "first-class",
+			price: { amount: 200, currency: "SEK" },
+		},
+	}
+
+	const leg2: model.Passenger.Itinerary.Leg = { ...leg, reference: "leg-1337", meal: [meal2] }
 	it("is", () => {
 		expect(model.Meal.is(meal)).toEqual(true)
 	})
@@ -18,5 +38,12 @@ describe("model.Flight.Meal", () => {
 	})
 	it("is reference", () => {
 		expect(model.Meal.is({ ...meal, reference: "ref-001" })).toEqual(true)
+	})
+
+	it("update passenger meal", () => {
+		expect(model.Meal.updatePassengerMeal(meal2, alternative, [leg, leg2], 0)).toEqual([meal2])
+	})
+	it("update passenger meal - deselect", () => {
+		expect(model.Meal.updatePassengerMeal({ ...meal2 }, alternative, [leg, leg2], 1)).toEqual([undefined])
 	})
 })
--- Meal/index.ts
+++ Meal/index.ts
@@ -1,3 +1,4 @@
+import { Itinerary } from "../Passenger/Itinerary"
 import { Alternative as MealAlternative } from "./Alternative"
 
 export interface Meal {
@@ -17,6 +18,36 @@ export namespace Meal {
 			Array.isArray(value.alternatives) &&
 			value.alternatives.every(Alternative.is)
 		)
+	}
+	export function updatePassengerMeal(
+		meal: Meal,
+		alternative: Meal.Alternative,
+		flights: Itinerary,
+		index: number
+	): Meal[] | undefined {
+		const itinerary: Itinerary | undefined = { ...flights }
+		const passengerMeals: Meal[] | undefined = index != undefined && itinerary ? itinerary[index].meal ?? [] : undefined
+
+		const existingMeal: Meal | undefined =
+			passengerMeals && meal ? passengerMeals.find(m => m.reference == meal.reference) : undefined
+
+		const newMeal = existingMeal
+			? existingMeal.alternatives.map(a => a).find(a => a == alternative)
+				? existingMeal
+				: { ...existingMeal, alternatives: [alternative] }
+			: { ...meal, alternatives: [alternative] }
+
+		const indexMeal = existingMeal ? passengerMeals?.findIndex(m => m.reference == newMeal.reference) : undefined
+
+		passengerMeals != undefined
+			? indexMeal != undefined
+				? newMeal.reference === existingMeal?.reference
+					? passengerMeals?.splice(indexMeal, 1)
+					: (passengerMeals[indexMeal] = newMeal)
+				: passengerMeals?.push(newMeal)
+			: []
+
+		return passengerMeals
 	}
 	export type Alternative = MealAlternative
 	export const Alternative = MealAlternative
