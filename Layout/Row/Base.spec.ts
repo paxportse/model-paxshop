6eb3bfa12ee7c7b6d88f083be0967d76b8d992f3
toli2003 <toli2003@student.miun.se>
2023-01-10 21:33:39 +0100

    Added row to setSeat

--- Layout/Row/Base.spec.ts
+++ Layout/Row/Base.spec.ts
@@ -40,6 +40,7 @@ describe("model.Flight.Row", () => {
 						price: { amount: 400, currency: "SEK", offer: 200 },
 						wide: true,
 						reference: "123",
+						toilet: true,
 					},
 					{
 						status: "available",
@@ -48,6 +49,7 @@ describe("model.Flight.Row", () => {
 						price: { amount: 400, currency: "SEK" },
 						wide: true,
 						reference: "123",
+						toilet: true,
 					},
 				],
 			},
